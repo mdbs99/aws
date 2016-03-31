ef196dee9dfaf42e0117d93894c5830ef5901138
almp1 <andremedeiros@asolutions.com.br>
2016-03-31 11:59:24 -0300

    Added files via upload

--- examples/ses/01/unit1.pas
+++ examples/ses/01/unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ExtCtrls, aws_client, aws_ses, math;
+  ExtCtrls, ComCtrls, aws_client, aws_ses, math;
 
 type
 
@@ -28,10 +28,14 @@ type
     Label4: TLabel;
     Label5: TLabel;
     Label6: TLabel;
-    Label7: TLabel;
     Label8: TLabel;
     memMessage: TMemo;
+    memRetorno: TMemo;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
     procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { private declarations }
   public
@@ -52,9 +56,9 @@ var
   oMessage: TSESMessage;
   FRegionSES: ISESRegion;
 begin
-  FRegionSES := TSESRegion.Create(
-    TAWSClient.Create(
-      TAWSCredentials.Create(edtAcessKeyId.Text, edtSecretKey.Text, True)
+  FRegionSES := TSESRegion.New(
+    TAWSClient.New(
+      TAWSCredentials.New(edtAcessKeyId.Text, edtSecretKey.Text, True)
     ), edtRegion.Text
   );
 
@@ -66,8 +70,17 @@ begin
   oMessage.Message:= memMessage.Text;
   oMessage.Format:=teFormato(ifthen(CheckBox1.Checked, 0, 1));
 
-  FRegionSES.SESObjects.SendEmail(oMessage);
+  FRegionSES.SESObjects.SendEmail(oMessage).Stream.SaveToFile('retorno.txt');
 
+  memRetorno.Lines.LoadFromFile('retorno.txt');
+
+  PageControl1.ActivePage := TabSheet2;
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  PageControl1.ActivePage := TabSheet1;
 end;
 
 end.
